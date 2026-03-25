ede55494ae3b362b6b6778c13b09cf405d578194
aeroldd <aeroldzeidparcon@gmail.com>
2026-03-25 17:31:12 +0400

    feat: dash after image vfx
    -implements previously created after image vfx
    -uses clean modular signal architecture
    -reworked and refactored dash controller so that it references flags and updates them -- may remove soon?? because signals seem better and more modular

---
+++ scenes/characters/player/dash_vfx.gd
@@ -0,0 +1,48 @@
+extends VFX
+
+var spawn_timer: float = 0
+@export var spawn_interval: float = 0.002
+
+@export var after_image: PackedScene
+@export var sprite: Sprite2D
+
+var active: bool = false
+
+# connect all the signals from the dash controller
+func _ready() -> void:
+	var dash = $"../../MovementController/MovementAbilities/DashAbility"
+	dash.dash_started.connect(_on_dash_started)
+	dash.dash_ended.connect(_on_dash_ended)
+	
+func _on_dash_started():
+	active = true
+	sprite.modulate.inverted()
+	
+func _on_dash_ended():
+	active = false
+	sprite.modulate.inverted()
+
+func update_vfx(delta):
+	if active:
+		spawn_timer -= delta
+		
+		if spawn_timer <= 0:
+			spawn_after_image()
+			#print(spawn_timer)
+			spawn_timer = spawn_interval
+	else:
+		return
+	
+func spawn_after_image():
+	var img = after_image.instantiate()
+	
+	img.global_position = sprite.global_position
+	img.global_rotation = sprite.global_rotation
+	
+	# copy texture and current frame
+	img.get_node("Sprite2D").texture = sprite.texture
+	img.get_node("Sprite2D").hframes = sprite.hframes
+	img.get_node("Sprite2D").frame = sprite.frame
+	img.get_node("Sprite2D").flip_h = sprite.flip_h
+	
+	get_tree().current_scene.add_child(img)
--- scripts/systems/DashController.gd
+++ scripts/systems/DashController.gd
@@ -8,7 +8,15 @@ var time
 
 @export var decay = 1000
 
+var spawn_timer = 0
+@export var spawn_interval: float = 0.05
+@export var after_image: PackedScene
+@export var sprite: Sprite2D
+
 var dash_velocity: Vector2 = Vector2.ZERO
+
+signal dash_started
+signal dash_ended
 	
 func calculate_dash_velocity(dash_strength: float, dash_angle: float) -> Vector2:
 	var new_velocity: Vector2
@@ -24,15 +32,32 @@ func check_dash_pressed(input_intent):
 # Calculates the changed dash velocity if conditions are met (dash cooldown reset and dash input pressed_
 func apply(input_intent, _velocity, delta):
 	if input_intent.dash_pressed:
-		dash_timer = dash_length
+		# Check if dash hasn't been pressed yet
+		if dash_timer == 0:
+			dash_timer = dash_length
+			
 		if dash_timer == dash_length: #just pressed dash
 			var dash_angle = input_intent.get_angle()
 			dash_velocity = calculate_dash_velocity(dash_strength, dash_angle)
 			return dash_velocity
+			
+	# Decriment dash timer
 	dash_timer = move_toward(dash_timer, 0, delta)
 	
-	#slow the dash down
+	# slow the dash down when not dashing
 	dash_velocity.x = move_toward(dash_velocity.x, 0, decay)
 	dash_velocity.y = move_toward(dash_velocity.y, 0, decay)
 	
 	return dash_velocity
+	
+func update(flags):
+	var was_dashing = flags.get("is_dashing", false)
+	var is_dashing = dash_timer != 0
+	
+	flags["is_dashing"] = is_dashing
+	
+	if is_dashing and not was_dashing:
+		emit_signal("dash_started")
+	
+	if not is_dashing and was_dashing:
+		emit_signal("dash_ended")
